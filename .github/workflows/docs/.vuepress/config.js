98e1183b3c14aadaaff7774b7138f3e2fc8e111f
Michael Nien <mobile.michael.nien@gmail.com>
2022-10-19 21:25:49 +0800

    新增 docs

---
+++ .github/workflows/docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Terraform 的一百零一種姿勢',
+  description: 'Terraform manage guide',
+  base: '/terraform-manage-guide/',
+  repo: 'https://github.com/shazi7804/terraform-manage-guide',
+  themeConfig: {
+    sidebarDepth: 0,
+    sidebar: [
+      ['/', '前言']
+    ],
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Github', link: 'https://github.com/shazi7804/terraform-manage-guide' }
+    ]
+  }
+}
