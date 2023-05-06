c0d71e1a99e66d4c4ad81d7174c6daad1350b8e3
Michael Nien <mobile.michael.nien@gmail.com>
2023-05-06 22:09:05 +0800

    調整文件資料設定

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,16 @@ module.exports = {
   themeConfig: {
     sidebarDepth: 0,
     sidebar: [
-      ['/', '首頁']
+      ['/', '首頁'],
+	        {
+        title: 'Group 1',   // 必要的
+        path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
     ],
     nav: [
       { text: '首頁', link: '/' },
