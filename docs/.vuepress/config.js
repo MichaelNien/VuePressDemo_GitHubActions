dee14670fb9aded3ff50f642e9c96f83721896b9
Michael Nien <mobile.michael.nien@gmail.com>
2023-05-06 22:13:48 +0800

    調整文件資料設定

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,19 +10,18 @@ module.exports = {
     sidebarDepth: 0,
     sidebar: [
       ['/', '首頁'],
-	        {
-        title: 'Group 1',   // 必要的
-        path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+	  {
+        title: 'Group 1',
+        collapsable: false,
+        sidebarDepth: 1,
         children: [
-          '/'
+          ['/demo/test.md', 'Test1']
         ]
       },
     ],
     nav: [
-      { text: '首頁', link: '/' },
-	  { text: 'Test1', link: '/demo/test.md' }
+      {text: '首頁', link: '/' },
+	  {text: 'Test1', link: '/demo/test.md' }
     ]
   }
 }
